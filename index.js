7a000e43a48cda328c9f09b3fc8e051358f517d6
Lincoln Araujo <lincoln.fa@hotmail.com>
2022-09-18 09:14:17 -0300

    creating index.html file

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const { url } = require('inspector');
 
 const server = express();
 
 server.use(express.json());
 
-const products = ["cup", "pillow","t-shirt","hat","bracelet"];
+const products = [
+    {
+        name: "Light Sandwich",
+        ingredients: ["bread", "lettuce", "onion", "eggs", "cheese", "cucumber","special sauce"],
+        price: 2.5,
+        image: url("./images/sandwich.jpg")
+    },
+    {
+        name: "Bread with Egg",
+        ingredients: ["bread", "egg", "butter"],
+        price: 1,
+        image: url("./images/bread_with_egg.jpg")
+    },
+    {
+        name: "Shrimp Salad",
+        ingredients: ["fried shrimp", "lettuce", "tomatoes", "bluberries", "arugula"],
+        price: 1,
+        image: url("./images/shrimp_salad.jpg")
+    }
+];
 
 // getting a product
 server.get('/products/:index', (req, res) => {
