c679f551cbf4d4b55248ebbce5965747a59997b2
Lincoln Araujo <72479081+Lincoln-Araujo@users.noreply.github.com>
2022-09-27 09:50:16 -0300

    Create gettingbuttons.js

---
+++ src/gettingbuttons.js
@@ -0,0 +1,5 @@
+const plusButton = document.getElementById('plus-button');
+
+const editButton = document.getElementById('edit-button');
+
+const deleteButton = document.getElementById('delete-button');
